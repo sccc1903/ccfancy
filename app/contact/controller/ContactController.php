2c44aab02a6cc6a2de7c964794b325c22812d817
lc <chen_chen1995@qq.com>
2017-10-19 21:52:45 +0800

    联系我们

--- app/contact/controller/ContactController.php
+++ app/contact/controller/ContactController.php
@@ -13,4 +13,16 @@ class ContactController extends HomeBaseController{
     public function index() {
         return $this->fetch(':index');
     }
+
+    public function collect(){
+        $data = $this->request->param();
+        $res = Db::name('contact')->insert($data['data']);
+        if($res){
+            $message['status'] = 1;
+            $message['msg'] = '提交成功我们会尽快和您联系';
+           echo json_encode($message);
+        }else{
+            return json(['status'=>0,'msg'=>'提交失败请查证您的数据']);
+        }
+    }
 }
